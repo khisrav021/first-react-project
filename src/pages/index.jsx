ce496e54881a578630b8eb11c11387e6d3ad82d0
khisrav <khisrav021@gmail.com>
2021-09-17 19:18:40 +0500

    added main page

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ManPage() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <small>test</small>
+        </div>
+    )
+}
+
+export default ManPage
